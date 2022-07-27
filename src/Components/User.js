0522b85aab6bbcc2fc6645f9af8f1a724fafa022
Siva <sivaiah.sankula@xcubelabs.com>
2022-07-27 21:20:59 +0530

    functional and class component

---
+++ src/Components/User.js
@@ -0,0 +1,15 @@
+
+import React,{Component} from "react";
+
+class User extends Component{
+  
+    render(){
+        return(
+            <div>
+                User - someUser age -22
+            </div>
+        )
+    }
+
+}
+export default User
